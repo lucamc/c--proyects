d11f3a60b85a7dd84c50ca56fa331d6dc6a95563
Lucas Custodio <lucas.custodio9416@gmail.com>
2022-10-07 10:55:53 -0300

    Add. proyects 74 and 75, vectors

---
+++ proyect74/proyect74/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyect74
+{
+    class SueldosOperarios
+    {
+        private float[] sueldos;
+
+
+        public void SolicitarSueldos()
+        {
+            int cantSueldos;
+            Console.Write("Cuantos sueldos va cargar: ");
+            cantSueldos = int.Parse(Console.ReadLine());
+            sueldos = new float[cantSueldos];
+        }
+
+
+
+        public void CargarSueldos()
+        {
+            for(int i = 0; i < sueldos.Length; i++)
+            {
+                Console.Write("Ingrese sueldo: ");
+                sueldos[i] = float.Parse(Console.ReadLine());
+
+            }
+        }
+
+
+
+        public void Imprimir()
+        {
+            for(int i = 0; i < sueldos.Length; i++)
+            {
+                Console.WriteLine("Sueldos: " + sueldos[i]);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            SueldosOperarios sueldos1 = new SueldosOperarios();
+            sueldos1.SolicitarSueldos();
+            sueldos1.CargarSueldos();
+            sueldos1.Imprimir();
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
---
+++ proyect75/proyect75/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyect75
+{
+    /* Desarrollar un programa que permita ingresar un vector de n elementos, 
+     * ingresar n por teclado. Luego imprimir la suma de todos sus elementos*/
+    class SumaValores
+    {
+        private float[] vectorValores;
+        private int cantidadValores;
+
+
+        public void CargaValores()
+        {
+            Console.Write("Ingrese cantidad de valores que desea sumar: ");
+            cantidadValores = int.Parse(Console.ReadLine());
+            vectorValores = new float[cantidadValores];
+
+            for(int i = 0; i < vectorValores.Length; i++)
+            {
+                Console.Write("Ingrese valor: ");
+                vectorValores[i] = float.Parse(Console.ReadLine());
+            }
+
+        }
+
+        public void SumarListaValores()
+        {
+            float acc = 0;
+            for(int i = 0; i < vectorValores.Length; i++)
+            {
+                acc += vectorValores[i];
+            }
+
+            Console.Write("La Suma de los valores es de: " + acc);
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            SumaValores sumaValores1 = new SumaValores();
+            sumaValores1.CargaValores();
+            sumaValores1.SumarListaValores();
+            Console.ReadKey();
+        }
+    }
+}
